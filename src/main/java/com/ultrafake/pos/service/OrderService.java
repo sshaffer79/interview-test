b24719428d3b0ce12bddfe0956cc5b5318ed1524
sshaffer13 <D0gwood_2277>
2017-10-02 14:10:34 -0400

    Fixed some logic bugs in OrderService

--- src/main/java/com/ultrafake/pos/service/OrderService.java
+++ src/main/java/com/ultrafake/pos/service/OrderService.java
@@ -8,6 +8,9 @@ import com.ultrafake.pos.model.OrderLineItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Service
 public class OrderService {
     @Autowired
@@ -15,6 +18,8 @@ public class OrderService {
 
     @Autowired
     private Items items;
+
+    private static BigDecimal SALES_TAX = new BigDecimal(0.06);
 
     public OrderService(OrdersDAO ordersDAO) {
         this.ordersDAO = ordersDAO;
@@ -30,16 +35,19 @@ public class OrderService {
 
         OrderLineItem lineItem = order.getLineItems()
                 .stream()
-                .filter(i -> i.getName() == itemName)
+                .filter(i -> i.getName().equals(itemName))
                 .findFirst()
-                .get();
+                .orElse(null);
         if (lineItem == null) {
             lineItem = new OrderLineItem(itemName, 1, item.getPrice());
             order.getLineItems().add(lineItem);
         } else {
             lineItem.setNumberOfItems(lineItem.getNumberOfItems() + 1);
-            lineItem.getTotalPrice().add(item.getPrice());
+            lineItem.setTotalPrice(lineItem.getTotalPrice().add(item.getPrice()));
         }
+
+        calculateTaxAndTotals(order);
+
         ordersDAO.update(order);
 
         return order;
@@ -48,13 +56,33 @@ public class OrderService {
     public Order removeLineItem(String orderId, String itemName) {
         Order order = ordersDAO.getOrder(orderId);
 
-        order.getLineItems().removeIf(i -> i.getName() == itemName);
+        order.getLineItems().removeIf(i -> i.getName().equals(itemName));
+
+        calculateTaxAndTotals(order);
+
         ordersDAO.update(order);
 
         return order;
     }
 
-    public void clear(String orderId) {
+    public Order clear(String orderId) {
         ordersDAO.clear(orderId);
+        return new Order(orderId);
+    }
+
+    public void calculateTaxAndTotals(Order order) {
+        BigDecimal subTotal = new BigDecimal(0.00).setScale(2, RoundingMode.CEILING);
+        for (OrderLineItem lineItem : order.getLineItems()) {
+            subTotal = subTotal.add(lineItem.getTotalPrice());
+        }
+        order.setSubTotal(subTotal);
+
+        BigDecimal salesTax = subTotal.multiply(SALES_TAX).setScale(2, RoundingMode.CEILING);
+        order.setSalesTax(salesTax);
+
+        BigDecimal total = new BigDecimal(0.00).setScale(2, RoundingMode.CEILING);
+        total = total.add(order.getSubTotal());
+        total = total.add(order.getSalesTax());
+        order.setTotal(total);
     }
 }
