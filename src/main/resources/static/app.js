2222faa70064568bd8eeb448067eddbcb1f630c5
sshaffer13 <D0gwood_2277>
2017-10-02 21:01:57 -0400

    Added console logging for JS and added an alert for pay not to say not implemented

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -5,17 +5,20 @@ fakePosModule.controller('orderEntryScreen', function($scope, $http) {
     $scope.selectedOrderItem;
     // Load Order
     $http.get('/api/order').then(function(response) {
+        console.dir(response.data);
         $scope.order = response.data;
     })
 
     $scope.$on('itemSelected', function(event, message) {
         $scope.$broadcast('addToOrder', message);
+        console.log("Select item " + message.id);
         $http.put('/api/order/item/'+message.id).then(function(response) {
+            console.dir(response.data);
             $scope.order = response.data;
         });
     });
 
-    $scope.itemSelect  = function(selectedOrderItem) {
+    $scope.orderItemSelect  = function(selectedOrderItem) {
         console.log("Selected item " + selectedOrderItem.name);
         $scope.selectedOrderItem = selectedOrderItem;
     };
@@ -23,6 +26,7 @@ fakePosModule.controller('orderEntryScreen', function($scope, $http) {
     $scope.itemVoided = function() {
         console.log("Void item " + $scope.selectedOrderItem.name);
         $http.delete('/api/order/item/'+$scope.selectedOrderItem.id).then(function(response) {
+            console.dir(response.data);
             $scope.order = response.data;
         })
     };
@@ -30,15 +34,14 @@ fakePosModule.controller('orderEntryScreen', function($scope, $http) {
     $scope.clear = function() {
         console.log("Clear order")
         $http.delete('/api/order/clear').then(function(response) {
+            console.dir(response.data);
             $scope.order = response.data;
         })
     };
 
     $scope.paynow = function() {
         console.log("Pay now")
-        $http.get('/api/order/pay').then(function(response) {
-            $scope.order = response.data;
-        })
+        alert("Not yet implemented!");
     };
 });
 
