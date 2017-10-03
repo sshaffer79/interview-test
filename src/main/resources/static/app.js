3458713be0f9279aea41254911a4b706911d53d2
sshaffer13 <D0gwood_2277>
2017-10-03 08:58:04 -0400

    Removed console output in app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -5,7 +5,6 @@ fakePosModule.controller('orderEntryScreen', function($scope, $http) {
     $scope.selectedOrderItem;
     // Load Order
     $http.get('/api/order').then(function(response) {
-        console.dir(response.data);
         $scope.order = response.data;
     })
 
@@ -13,27 +12,23 @@ fakePosModule.controller('orderEntryScreen', function($scope, $http) {
         $scope.$broadcast('addToOrder', message);
         console.log("Select item " + message.id);
         $http.put('/api/order/item/'+message.id).then(function(response) {
-            console.dir(response.data);
             $scope.order = response.data;
             location.reload();
         });
     });
 
     $scope.orderItemSelect  = function(selectedOrderItem) {
-        console.log("Selected item " + selectedOrderItem.name);
         $scope.selectedOrderItem = selectedOrderItem;
     };
 
     $scope.itemVoided = function() {
         console.log("Void item " + $scope.selectedOrderItem.name);
         $http.delete('/api/order/item/'+$scope.selectedOrderItem.id).then(function(response) {
-            console.dir(response.data);
             $scope.order = response.data;
         })
     };
 
     $scope.clear = function() {
-        console.log("Clear order")
         $http.delete('/api/order/clear').then(function(response) {
             console.dir(response.data);
             $scope.order = response.data;
@@ -41,7 +36,6 @@ fakePosModule.controller('orderEntryScreen', function($scope, $http) {
     };
 
     $scope.paynow = function() {
-        console.log("Pay now")
         alert("Not yet implemented!");
     };
 });
